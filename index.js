cca682d135c4ff311712a0bdebe502a300c79455
Olle Wistedt <wistedt@live.com>
2025-04-15 15:20:46 +0200

    Fixed minor bug causing nav-toggle to malfunction

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// Jag har fått hjälp med den här och kan inte javascripten själv, men förstår att den växlar mellan att lägga till och ta bort .showMenu-klassen efter .navlinks-klassen i HTML.
-// Det i sin tur växlar mellan att "skriva över" display:none; med display:flex i CSS.
-function myFunction() {
-  var x = document.getElementById("nav");
-  if (x.className === "navbar") {
-    x.className += "showMenu";
-  } else {
-    x.className = "navbar";
-  }
+const toggle = () => {
+  document.querySelector("#nav").classList.toggle("showMenu");
 }
-
-// const toggle = () => {
-//   let x = document.querySelector("#nav");
-//   if (x.className === "navbar") {
-//     x.className += "show menu"
-//   } else {
-//     x.className = "navbar";
-//   }
-// }
-
